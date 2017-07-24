da3050b473a1bf9991307c1d87cb37934d1c44fa
Odiesha Wallace <p_o_w2@hotmail.com>
2017-07-24 10:24:50 -0500

    Removed line 17 from app.js

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ const express = require('express'),
 
 var app = express();
 
-
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-// view engine setup
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(bodyParser.json());
 app.use(express.static(srcDir));
