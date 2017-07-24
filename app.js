aaff7d58ec0ccf374b106fda2901faf6ae2c7f8b
Trainer <trainer@mwh.gov.local>
2017-07-24 11:09:48 -0500

    Added comment-Ruth

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+//Ruthii was here
 'use strict'
+
 const express = require('express'),
       path = require('path'),
       favicon = require('serve-favicon'),
@@ -14,7 +16,7 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(bodyParser.json());
 app.use(express.static(srcDir));
 app.set('port', process.env.PORT || 3000);
