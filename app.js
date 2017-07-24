7a5d29e44cc244452382560403597afb1450a6fa
Trainer <trainer@mwh.gov.local>
2017-07-24 10:33:53 -0500

    Removed COmment Vanroy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+
 app.use(bodyParser.json());
 app.use(express.static(srcDir));
 app.set('port', process.env.PORT || 3000);
