8eb68475a3460a3905da94ee845b6e9fc862e2a3
Odiesha Wallace <p_o_w2@hotmail.com>
2017-07-27 10:49:01 -0500

    Query data for aquisition table

--- models/acquisition.js
+++ models/acquisition.js
@@ -3,8 +3,17 @@
 const model = require('./base').model
 
 const SQL = {
+<<<<<<< HEAD
     GET: 'SELECT title, location FROM  acquisition '
 }
+=======
+    GET: 'SELECT  a.title, p.location, o.first_name, o.last_name, r.amount ' +
+         'FROM acquisitions as a ' +  
+         'INNER JOIN owner AS o ON a.id = o.accession_id ' +
+         'INNER JOIN property AS p ON o.id = p.owner_id ' +
+         'INNER JOIN receipt AS r ON o.id = r.owner_id ' 
+}	
+>>>>>>> 5d321b8f37342db9fc600d5169e253aebce1ad35
 
 exports.index = (cb)=>{
   model.query(SQL.GET, (err, acquisitions)=>{
@@ -12,3 +21,4 @@ exports.index = (cb)=>{
     cb(null, acquisitions)
  	})
 }
+
