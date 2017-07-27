5d321b8f37342db9fc600d5169e253aebce1ad35
Jermaine Coates <jermainecoates@outlook.com>
2017-07-27 10:16:07 -0500

    Pull information from database

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api', acquisitions)
 // app.set('port', process.env.PORT || 8080);
 
 // var server = app.listen(app.get('port'), function() {
-  // console.log('Express server listening on port ' + server.address().port);
+//   console.log('Express server listening on port ' + server.address().port);
 // });
 
 module.exports = app;
--- models/acquisition.js
+++ models/acquisition.js
@@ -3,8 +3,12 @@
 const model = require('./base').model
 
 const SQL = {
-    GET: 'SELECT * FROM property'
-}
+    GET: 'SELECT  a.title, p.location, o.first_name, o.last_name, r.amount ' +
+         'FROM acquisitions as a ' +  
+         'INNER JOIN owner AS o ON a.id = o.accession_id ' +
+         'INNER JOIN property AS p ON o.id = p.owner_id ' +
+         'INNER JOIN receipt AS r ON o.id = r.owner_id ' 
+}	
 
 exports.index = (cb)=>{
   model.query(SQL.GET, (err, acquisitions)=>{
@@ -12,3 +16,4 @@ exports.index = (cb)=>{
     cb(null, acquisitions)
  	})
 }
+
